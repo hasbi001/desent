3b44f49efde07c559e65e615370dbda225f8167a
Hasbi Ali Firdaus <hasbialifirdaus001@gmail.com>
2026-02-28 14:27:26 +0700

    Change response status from 200 to 201 on book creation

--- app/controllers/bookController.js
+++ app/controllers/bookController.js
@@ -34,7 +34,7 @@ exports.create = (req, res) => {
 
     Book.create(data)
     .then(data => {
-      res.status(200).send(data);
+      res.status(201).send(data);
     })
     .catch(err => {
       res.status(500).send({
