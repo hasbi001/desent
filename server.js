5c213779d8111a169560cbf3dea6afd10345306a
Hasbi Ali Firdaus <hasbialifirdaus001@gmail.com>
2026-02-28 14:07:52 +0700

    update sync db

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
 const db = require("./app/models");
 const Role = db.role;
 
-db.sequelize.sync().then(() => {
+db.sequelize.sync({force: true}).then(() => {
   console.log('Drop and Resync Db');
 //   initial();
 });
